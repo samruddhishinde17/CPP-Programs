4aa722a3a7c2b87862d40f5785162c0b0302f946
Samruddhi Shinde <114212065+samruddhishinde17@users.noreply.github.com>
2025-03-06 16:55:50 +0530

    Add files via upload

---
+++ program192.cpp
@@ -0,0 +1,15 @@
+// Write a program to display "Jay Ganesh..." on screen.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
+
+// g++ program192.cpp -o Myexe
+// Myexe.exe
+// ./Myexe
---
+++ program193.cpp
@@ -0,0 +1,22 @@
+// Write a program to display addition of 2 no.
+// Approch 1
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0, iAns= 0;
+
+    cout<<"Enter first no = "<<endl;
+    cin>>iValue1;
+
+    cout<<"Enter second no = "<<endl;
+    cin>>iValue2;
+
+    iAns = iValue1 + iValue2;
+
+    cout<<"Addition is = "<<iAns<<endl;
+
+    return 0;
+}
